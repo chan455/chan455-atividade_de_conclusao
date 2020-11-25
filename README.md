dd798b31ee5e5f7c7d532e75cdf2cb74088f480b
Laercio Marques Lopes <leon.evil.4@gmail.com>
2020-11-25 19:14:37 -0300

    conclusao

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Atividade de Conclusão de Curso."
---
+++ app/views/home/index.php
@@ -0,0 +1,172 @@
+<?php 
+    $this->view('home/cabecario');
+?>
+        <!-- Navigation-->
+        <?php
+            $this->view('home/menu');
+        ?>
+        <!-- Page Content-->
+        <div class="container-fluid p-0">
+            <!-- About-->
+            <section class="resume-section" id="about">
+                <div class="resume-section-content">
+                    <h1 class="mb-0">
+                        Laércio 
+                        <span class="text-primary">Lopes</span>
+                    </h1>
+                    <div class="subheading mb-5">
+                        Rua São João 30 · Santa Catarina - Breu Branco-PA, cep:68488-000 · (94) 98802-1015 ·
+                        <a href="mailto:leon.evil@gmail.com">leon.evil@gmail.com</a>
+                    </div>
+                    <p class="lead mb-5">Sou granduando em serviço social, e declaradamente apoixado por telecomunicações, sou comunicativo e disposto sempre a aprender.</p>
+                    <div class="social-icons">
+                        <a class="social-icon" href="#"><i class="fab fa-linkedin-in"></i></a>
+                        <a class="social-icon" href="#"><i class="fab fa-github"></i></a>
+                        <a class="social-icon" href="#"><i class="fab fa-twitter"></i></a>
+                        <a class="social-icon" href="#"><i class="fab fa-facebook-f"></i></a>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Experience-->
+            <section class="resume-section" id="experience">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Experiencias</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Proprietário e Técnico</h3>
+                            <div class="subheading mb-3">Prime Informática</div>
+                            <p>Recem ativa a Prime Informática oferece o melhor suporte técnico em manutenção em notebooks impressoras e aparelhos eleetrônicos e somos especialistas em serviços de rede</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">Maio - Atual</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Assistente Técnico</h3>
+                            <div class="subheading mb-3">MundialNet - Telecom</div>
+                            <p>Organização da equipe de campo onde desde a distribuição de material para utilização em campo com suporte por call center durante a instalação para liberação do acesso a internet do cliente recem instalado.</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">Abril 2017 - Maio 2020</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Auxiliar Administrativo</h3>
+                            <div class="subheading mb-3">Vicare - Incorporadora e Loteamentos</div>
+                            <p> Organização de propostas e corretores para um melhor desempenho nas vendas, e organização do ambiente de trabalho.</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">Setembro 2012 - Junho 2013</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Auxiliar Administrativo</h3>
+                            <div class="subheading mb-3">Vicare - Incorporadora e Loteamentos</div>
+                            <p>Organização de propostas e corretores para um melhor desempenho nas vendas, e organização do ambiente de trabalho.</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">Setembro 2011 - Julho 2012</span></div>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Education-->
+            <section class="resume-section" id="education">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Educação</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">UNIASSELVI - Centro Universitário Leonardo Da Vinci.</h3>
+                            <div class="subheading mb-3"> Graduando Bacharel - Serviço Social</div>
+                            <div>Politica Públicas</div>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">Julho 2018 - dezembro 2021</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Ifto</h3>
+                            <div class="subheading mb-3">Desemvolvedor Web Design</div>
+                            </div>
+                        <div class="flex-shrink-0"><span class="text-primary">Augosto 2020 - Novembro 2020</span></div>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Skills-->
+            <section class="resume-section" id="skills">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Habilidades</h2>
+                    <div class="subheading mb-3">Programming Languages & Tools</div>
+                    <ul class="list-inline dev-icons">
+                        <li class="list-inline-item"><i class="fab fa-html5"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-css3-alt"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-js-square"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-angular"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-react"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-node-js"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-sass"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-less"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-wordpress"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-gulp"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-grunt"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-npm"></i></li>
+                    </ul>
+                    <div class="subheading mb-3">Fluxo de Trabalho</div>
+                    <ul class="fa-ul mb-0">
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Fake Mobile-First, Responsive Design
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Fake Cross Browser Testing & Debugging
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Fake Cross Functional Teams
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Fake Agile Development & Scrum
+                        </li>
+                    </ul>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Interests-->
+            <section class="resume-section" id="interests">
+                <div class="resume-section-content">
+                <?php 
+                    $this->view('home/form');
+                ?>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Awards-->
+            <section class="resume-section" id="awards">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Awards & Certifications</h2>
+                    <ul class="fa-ul mb-0">
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            Estrutura em Rede Optica - Lan Core - 2019
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            Tecnico em Edificações - CTP - 2011
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            
+                            Movimentador de Produtos Perigosos - Senai 2010
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            <sup></sup>
+                            Mecânico Ajustador - Senai - 2010 
+                        </li>
+                    </ul>
+                </div>
+            </section>
+        </div>
+       
+<?php 
+    $this->view('home/rodape');
+?>
